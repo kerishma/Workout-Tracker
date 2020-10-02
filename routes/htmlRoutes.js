f60dbbe77112367f70148b4ea1164ed28e6d2b3d
kerishma tarin <kerishma.tarin@gmail.com>
2020-10-01 18:47:49 -0700

    added routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = function (app) {
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
     app.get("/",function(req,res){    
         res.sendFile(path.join(__dirname,"../public/index.html"));
+    });
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
